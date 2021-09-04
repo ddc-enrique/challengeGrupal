70e862d25d2c602df6b2fe41e12b67e30b837b4e
enrique-dc93 <51374100+enrique-dc93@users.noreply.github.com>
2021-09-04 18:51:19 -0300

    commit despues de mergear Sabado a la tarde

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            
+    <nav>
+        <section className="nav navBar">
+            <article className="contact">
+                <img className="rrssImg" src="../assets/rrss_icons/pin.png"/>
+                <p>Av. Patricio peralta Ramos 2877, Mar del Plata, Buenos Aires</p>
+            </article>
+            <article className="contact">
+                <img className="rrssImg" src="../assets/rrss_icons/telephone.png"/>
+                <p>+ 54 223 5391098</p>
+            </article>
+            <article className="contact">
+                <img className="rrssImg" src="../assets/rrss_icons/mail.png"/>
+                <p>info@mardelcasas.com</p>
+            </article>
+            <article className="contact">
+                <img className="rrssImg" src="../assets/rrss_icons/facebook.png"/>
+                <img className="rrssImg" src="../assets/rrss_icons/instagram.png"/>
+                <img className="rrssImg" src="../assets/rrss_icons/twitter.png"/>
+            </article>
+        </section>
+        <div className="copyright">
+            <p> Copyright Cohort 21 | MindHub</p>
         </div>
+    </nav>
     )
 }
 
--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <div className="containerHome">
       <Hero />
+      <Footer />
     </div>
   );
 };
