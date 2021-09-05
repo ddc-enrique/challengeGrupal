10a8875505e99aa0d4f3b3b51be366ea26466c08
enrique-dc93 <51374100+enrique-dc93@users.noreply.github.com>
2021-09-04 21:19:37 -0300

    arreglado despues mergear, antes de arrancar el navbar

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,30 +2,38 @@ import React from 'react'
 
 const Footer = () => {
     return (
-    <nav>
-        <section className="nav navBar">
-            <article className="contact">
-                <img className="rrssImg" src="../assets/rrss_icons/pin.png"/>
-                <p>Av. Patricio peralta Ramos 2877, Mar del Plata, Buenos Aires</p>
-            </article>
-            <article className="contact">
-                <img className="rrssImg" src="../assets/rrss_icons/telephone.png"/>
-                <p>+ 54 223 5391098</p>
-            </article>
-            <article className="contact">
-                <img className="rrssImg" src="../assets/rrss_icons/mail.png"/>
-                <p>info@mardelcasas.com</p>
-            </article>
-            <article className="contact">
-                <img className="rrssImg" src="../assets/rrss_icons/facebook.png"/>
-                <img className="rrssImg" src="../assets/rrss_icons/instagram.png"/>
-                <img className="rrssImg" src="../assets/rrss_icons/twitter.png"/>
-            </article>
-        </section>
+    <footer>
+        <div className="contact">
+            <div class="contactInfo">
+                <div >
+                    <img className="rrssImg" src="/assets/rrss_icons/pin.png"/>
+                    <p>Alvarado 1560, Mar del Plata, Buenos Aires</p>
+                </div>
+                <div >
+                    <img className="rrssImg" src="/assets/rrss_icons/telephone.png"/>
+                    <p>+ 54 223 5391098</p>
+                </div>
+                <div >
+                    <img className="rrssImg" src="/assets/rrss_icons/mail.png"/>
+                    <p>info@mardelcasas.com</p>
+                </div>
+            </div>
+            <div className="socialNetworks">
+                <a href="">
+                    <img className="rrssImg" src="/assets/rrss_icons/facebook.png"/>
+                </a>
+                <a href="">
+                    <img className="rrssImg" src="/assets/rrss_icons/instagram.png"/>
+                </a>
+                <a href="">
+                    <img className="rrssImg" src="/assets/rrss_icons/twitter.png"/>
+                </a>
+            </div>
+        </div>
         <div className="copyright">
             <p> Copyright Cohort 21 | MindHub</p>
         </div>
-    </nav>
+    </footer>
     )
 }
 
