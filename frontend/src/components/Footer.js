06d2e4351b965bee741a0c7ff8729aec7769ccfd
enrique-dc93 <51374100+enrique-dc93@users.noreply.github.com>
2021-09-04 21:42:47 -0300

    con styles dividos por componentes, antes de arrancar navbar

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,32 +1,33 @@
+import "../styles/Footer.css"
 import React from 'react'
 
 const Footer = () => {
     return (
     <footer>
         <div className="contact">
-            <div class="contactInfo">
+            <div className="contactInfo">
                 <div >
-                    <img className="rrssImg" src="/assets/rrss_icons/pin.png"/>
+                    <img className="rrssImg" src="/assets/rrss_icons/pin.png" alt="pin"/>
                     <p>Alvarado 1560, Mar del Plata, Buenos Aires</p>
                 </div>
                 <div >
-                    <img className="rrssImg" src="/assets/rrss_icons/telephone.png"/>
+                    <img className="rrssImg" src="/assets/rrss_icons/telephone.png" alt="telephone"/>
                     <p>+ 54 223 5391098</p>
                 </div>
                 <div >
-                    <img className="rrssImg" src="/assets/rrss_icons/mail.png"/>
+                    <img className="rrssImg" src="/assets/rrss_icons/mail.png" alt="email"/>
                     <p>info@mardelcasas.com</p>
                 </div>
             </div>
             <div className="socialNetworks">
-                <a href="">
-                    <img className="rrssImg" src="/assets/rrss_icons/facebook.png"/>
+                <a href="www.facebook.com" target="_blank">
+                    <img className="rrssImg" src="/assets/rrss_icons/facebook.png" alt="facebook"/>
                 </a>
-                <a href="">
-                    <img className="rrssImg" src="/assets/rrss_icons/instagram.png"/>
+                <a href="www.instagram.com" target="_blank">
+                    <img className="rrssImg" src="/assets/rrss_icons/instagram.png" alt="instagram"/>
                 </a>
-                <a href="">
-                    <img className="rrssImg" src="/assets/rrss_icons/twitter.png"/>
+                <a href="www.twitter.com" target="_blank">
+                    <img className="rrssImg" src="/assets/rrss_icons/twitter.png" alt="twitter"/>
                 </a>
             </div>
         </div>
