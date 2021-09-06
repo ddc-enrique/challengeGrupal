ce721e65e4ddb6169894ef8456ca090776efe889
Diego Iglesias <diegoiglesiasrojo@gmail.com>
2021-09-06 16:44:39 -0300

    validator messages fixed

--- controllers/validatorControllers.js
+++ controllers/validatorControllers.js
@@ -6,13 +6,13 @@ const validatorControllers ={
                 "string.max": "Se permite un máximo de 35 caracteres",
                 "string.min": "Se requiere un mínimo de 2 caracteres",
                 "string.trim": "No se permiten espacios antes y despues del nombre",
-                "string.pattern.base": "El nombre no es valido"
+                "string.pattern.base": "No se permiten números"
             }),
             lastName: joi.string().trim().min(2).max(35).pattern(new RegExp('[^0-9]+$')).required().messages({
                 "string.max": "Se permite un maximo de 35 caracteres",
                 "string.min": "Se requiere un mínimo de 2 caracteres",
                 "string.trim": "No se permiten espacios antes y despues del apellido",
-                "string.pattern.base": "El apellido no es valido"
+                "string.pattern.base": "No se permiten números"
             }),
             eMail: joi.string().trim().min(6).max(255).email().required().messages({
                 "string.max": "Se permite un maximo de 255 caracteres",
@@ -37,32 +37,39 @@ const validatorControllers ={
         const validation = schema.validate(req.body, {abortEarly: false})
         if(!validation.error){
             next()
-        }else{ // ver como manejar errores en front
-            res.json({success: false, error: validation.error.details})
+        }else{
+            res.json({success: false, errors: validation.error.details})
         }
     },
     validatorPasswordChange : (req, res, next) =>{
         const schema = joi.object({
-            password: joi.string().trim().min(4).max(255).required(),
+            password: joi.string().trim().min(4).max(255).required().messages({
+                "string.max": "Se permite un maximo de 255 caracteres",
+                "string.min": "Se requiere un mínimo de 4 caracteres",
+                "string.trim": "No se permiten espacios antes y despues de la contraseña",
+            }),
         })
         const validation = schema.validate(req.body, {abortEarly: false})
         if(!validation.error){
             next()
-        }else{ // ver como manejar errores en front
-            let validationStringified = validation.error.details.map(element => element.message).join(' ')
-            res.json({success: false, response: validationStringified})
+        }else{
+            res.json({success: false, errors: validation.error.details})
         }
     },
     validatorPasswordResetEmailSend : (req, res, next)=>{
         const schema = joi.object({
-            eMail: joi.string().trim().min(6).max(255).email().required(),
+            eMail: joi.string().trim().min(6).max(255).email().required().messages({
+                "string.max": "Se permite un maximo de 255 caracteres",
+                "string.min": "Se requiere un mínimo de 6 caracteres",
+                "string.trim": "No se permiten espacios antes y despues del email",
+                "string.email": "Se debe ingresar un email valido"
+            }),
         })
         const validation = schema.validate(req.body, {abortEarly: false})
         if(!validation.error){
             next()
-        }else{ // ver como manejar errores en front
-            let validationStringified = validation.error.details.map(element => element.message).join(' ')
-            res.json({success: false, response: validationStringified})
+        }else{
+            res.json({success: false, errors: validation.error.details})
         }
     }
 }
