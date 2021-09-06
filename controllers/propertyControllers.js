54bbdabae78e52613f094c7547a4bd252c2b939f
WernerBudtke <68172417+WernerBudtke@users.noreply.github.com>
2021-09-05 23:40:52 -0300

    added reset password functionality, validate user, disable user when he didn't use password, all things are mailed to user, mail on new posted city with equal filter saved on user, save filter to user when no city are found after he does a filter, added isUSD to prop model, added enpoints for every new feature and also some related to admin features like ban unban, added enpoint to count number props from a city

--- controllers/propertyControllers.js
+++ controllers/propertyControllers.js
@@ -1,10 +1,23 @@
 const Property = require('../models/Property')
+const User = require('../models/User')
+const nodemailer = require('nodemailer')
 const handleError = (res,err) =>{
     console.log(err.message)
     res.json({success: false, response: err.message})
 }
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+    type: 'OAuth2',
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+})
 const propertyControllers = {
-    getProperties:(req, res) =>{
+    getProperties: (req, res) => {
         // necesito que me manden de frontend todos los filtros dentro de body en {filter:{filtros...}} OJO dentro de filtros tener greater and lower
         console.log("Received Get Properties Petition:" + Date())
         if (req.body.filter){
@@ -21,24 +34,74 @@ const propertyControllers = {
             res.json({sucess: false, response: "Body can't be blank, have to put something"})
         }    
     },
-    getAProperty:(req, res) =>{
+    getAProperty: (req, res) => {
         console.log("Received Get Property Petition:" + Date())
         Property.findOne({_id: req.params.id})
         .then(property => res.json({success: true, response: property}))
         .catch(err => handleError(res,err))
     },
-    addAProperty:(req, res) =>{ 
+    addAProperty: (req, res) => { 
         console.log("Received Post Property Petition:" + Date())
         if(req.user.admin){
             const newProperty = new Property({...req.body})
+            const {isBrandNew, isHouse, forSale, haveGarden, haveGarage, havePool, numberOfBathrooms, numberOfBedrooms, numberOfRooms, roofedArea, totalArea, price} = req.body
+            const whatToSearchFor = {
+                isBrandNew,
+                isHouse,
+                forSale,
+                haveGarden,
+                haveGarage,
+                havePool,
+                numberOfBathrooms,
+                numberOfBedrooms,
+                numberOfRooms
+            } // hay que acordar QUE COSAS se guardan en el filtro del usuario cuando se agrega al usuario, para buscar ACA
+            // console.log(whatToSearchFor)
             newProperty.save()
-            .then(property => res.json({success: true, response: property}))
+            .then((property) => {
+                User.find({dreamProperty: whatToSearchFor, suscribedToNewsLetter: true})
+                .then(async users => {
+                    if(users.length > 0){
+                        let emailsAccepted = []
+                        let emailsRejected = []
+                        for ( const user of users){
+                            let message = `
+                            <h1>Hello ${user.firstName} ${user.lastName}</h1>
+                            <p>We would like to inform you that your dreamed house is on our page now:</p>
+                            <break></break>
+                            <a href="http://localhost:3000/house/${property._id}">CLICK HERE!</a>
+                            `//reemplazar esta URL por una de frontend, que vaya en params un ID, que en front monte componente y useEffect did mount, haga pedido a esa ruta de api con el req params id
+                            let mailOptions = {
+                                from: "Mar Del Casas <mardelcasas@gmail.com>",
+                                to: `${user.firstName} <${user.eMail}>`,
+                                subject: `Your dreamed house ${user.firstName}!`,
+                                text: message,
+                                html: message
+                            }
+                            try{
+                                let response = await transporter.sendMail(mailOptions)
+                                if(response.accepted){
+                                    emailsAccepted.push(response.accepted[0])
+                                }else{
+                                    emailsRejected.push(response.rejected[0])
+                                }
+                            }catch(err){
+                                throw new Error("wasn't able to send emails")
+                            }
+                        }                     
+                        res.json({success: true, response: `Added house and Sent mail to: ${emailsAccepted.join(', ')} Rejected: ${emailsRejected.length > 0 ? emailsRejected.join(', ') : 0}`})
+                    }else{
+                        res.json({success: true, response: "Added house but didn't find any user to send mail"})
+                    }
+                })
+                .catch(err => handleError(res, err))
+            })
             .catch(err => handleError(res,err))
         }else{
             res.json({success: false, response: "You don't have permissions"})
         }   
     },
-    removeAProperty:(req, res) =>{
+    removeAProperty: (req, res) => {
         console.log("Received Remove Property Petition:" + Date())
         if(req.user.admin){
             Property.findOneAndDelete({_id: req.body._id})
@@ -50,7 +113,7 @@ const propertyControllers = {
             res.json({success: false, response: "You don't have permissions"})
         }
     },
-    modifyAProperty:(req, res)=>{
+    modifyAProperty: (req, res) => {
         console.log("Received Modify Property Petition:" + Date())
         if(req.user.admin){
             Property.findOneAndUpdate({_id: req.body._id}, {...req.body}, {new:true})
@@ -61,6 +124,19 @@ const propertyControllers = {
         }else{
             res.json({success: false, response: "You don't have permissions"})
         }
+    },
+    updateManyProps: (req, res) => {
+        console.log("Received Update Many Properties Petition:" + Date())
+        Property.updateMany({}, {...req.body})
+        .then(() => res.json({success: true, response: "updated all properties"}))
+        .catch(err => res.json({success: false, response: err}))
+    },
+    getNumberOfProps: (req, res) => {
+        console.log("Received Get Number Of Properties Petition:" + Date())
+        Property.countDocuments({city:req.params.id})
+        .then(number => res.json({success: true, response: number}))
+        .catch(err => handleError(res, err))
     }
+
 }
 module.exports = propertyControllers
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -6,6 +6,18 @@ const handleError = (res,err) =>{
     console.log(err.message)
     res.json({success: false, response: err.message})
 }
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+    type: 'OAuth2',
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+})
+
 const userControllers = {
     registerUser: (req, res) =>{
         console.log("Received Register User Petition:" + Date())
@@ -23,7 +35,7 @@ const userControllers = {
         newUser.save()
         .then(user => {
             const token = jwt.sign({...newUser}, process.env.SECRETORKEY)
-            res.json({success: true, response: user, token: token})
+            res.json({success: true, response: user, token: token}) // desde front, tomar token y pegarle al validar mail
         })
         .catch(err => {
             res.json({success: false, response: err.message.includes('duplicate key') ? 'eMail already in use' : err.message})
@@ -40,6 +52,12 @@ const userControllers = {
                     if((userFound.google === true && google === false) || (userFound.facebook === true && facebook === false)){
                         throw new Error(`Log in with ${userFound.google ? 'Google' : 'Facebook'}!`)
                     }
+                    if(!userFound.validated){
+                        throw new Error(`Please validate your account, check your email`) // que en front end al recibir este error, de opcion de enviar mail de nuevo.
+                    }
+                    if(userFound.banned){
+                        throw new Error('This user has a ban on him')
+                    }
                     if(!bcryptjs.compareSync(password, userFound.password))throw new Error(errMessage)
                     const token = jwt.sign({...userFound}, process.env.SECRETORKEY) 
                     res.json({success: true, response: userFound, token})
@@ -51,48 +69,115 @@ const userControllers = {
         })
         .catch(err => handleError(res, err))   
     },
-    sendMail: (req, res)=>{
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-            type: 'OAuth2',
-            user: process.env.MAIL_USERNAME,
-            pass: process.env.MAIL_PASSWORD,
-            clientId: process.env.OAUTH_CLIENTID,
-            clientSecret: process.env.OAUTH_CLIENT_SECRET,
-            refreshToken: process.env.OAUTH_REFRESH_TOKEN
-            }
-        })
+    sendValidationMail: (req, res)=>{
+        console.log("Received Send Validation Mail Petition:" + Date())
+        let message = `
+            <h1>Hello ${req.user.firstName} ${req.user.lastName}</h1>
+            <p>Please to confirm your account continue to this link:</p>
+            <break></break>
+            <a href="http://localhost:4000/api/user/validatemail/${req.user._id}">CLICK HERE!</a>
+        `//reemplazar esta URL por una de frontend, que vaya en params un ID, que en front monte componente y useEffect did mount, haga pedido a esa ruta de api con el req params id
         let mailOptions = {
             from: "Mar Del Casas <mardelcasas@gmail.com>",
             to: `${req.user.firstName} <${req.user.eMail}>`,
             subject: `Welcome ${req.user.firstName}!`,
-            text: `
-                <h1>Hello ${req.user.firstName} ${req.user.lastName}</h1>
-                <p>Please to confirm your account continue to this link:</p>
-                <break></break>
-                <a href="http://localhost:4000/api/user/validatemail/${req.user._id}">CLICK HERE!</a>
-            `,
-            html: `
-                <h1>Hello ${req.user.firstName} ${req.user.lastName}</h1>
-                <p>Please to confirm your account continue to this link:</p>
-                <break></break>
-                <a href="http://localhost:4000/api/user/validatemail/${req.user._id}">CLICK HERE!</a>
-            `
+            text: message,
+            html: message
         }
-        transporter.sendMail(mailOptions, function(err, data) {
-            if (err) {
-            console.log("Error " + err);
-            res.json({success: false, response: err})
-            } else {
-            console.log("Email sent successfully")
-            res.json({success: true, response: data})
-            }
+        transporter.sendMail(mailOptions, (err, data) => {
+            err ? res.json({success: false, response: err}) : res.json({success: true, response: data})
         })
     },
     validateUser: (req, res)=>{
-        console.log(req.params.id)
-        res.json({success: true, response: req.params.id})
+        console.log("Received Validate User Email Petition:" + Date())
+        User.findOneAndUpdate({_id: req.params.id}, {validated: true})
+        .then(user => user ? res.json({success: true}) : res.json({success: false, response: "Didn't find that user"}))
+        .catch(err => handleError(res, err))
+    },
+    sendResetPasswordMail: (req, res) =>{
+        console.log("Received Send Reset Password Mail Petition:" + Date())
+        const {eMail} = req.body
+        User.findOne({eMail: eMail})
+        .then(user => {
+            if(user){
+                if(!user.validated){
+                    throw new Error("Have to validate that user first")
+                }
+                let message = `
+                    <h1>Hello ${user.firstName} ${user.lastName}</h1>
+                    <p>Please to change your password continue to this link:</p>
+                    <break></break>
+                    <a href="http://localhost:4000/api/user/resetpassword/${user._id}">CLICK HERE!</a>
+                `//mandarlo a frontend a una pagina con 2 input para la contraseña y que cuando el tipo toque enviar le pegues a ese endpoint, con ese params id y el paquete en el body
+                let mailOptions = {
+                    from: "Mar Del Casas <mardelcasas@gmail.com>",
+                    to: `${user.firstName} <${user.eMail}>`,
+                    subject: `Reset Password ${user.firstName}!`,
+                    text: message,
+                    html: message
+                }
+                transporter.sendMail(mailOptions, (err, data) => {
+                    err ? res.json({success: false, response: err}) : res.json({success: true, response: data})
+                })
+            }else{
+                throw new Error("Didn't find that user")
+            }
+        })
+        .catch( err => handleError(res, err))
+    },
+    resetUserPassword: (req, res)=>{ // desde el punto de vista de la inseguridad... es medio inseguro esto, ver como hacer.
+        console.log("Received Reset Password Petition:" + Date())
+        const {password} = req.body
+        let hashedPass = bcryptjs.hashSync(password)
+        User.findOneAndUpdate({_id: req.params.id}, {password: hashedPass})
+        .then(user => {
+            if(user){
+                let message = `
+                    <h1>Hello ${user.firstName} ${user.lastName}</h1>
+                    <p>We would like to inform you, your password has been successfully reset!</p>
+                    <p>If you didn't change your password, and you want to disable your account please continue and click the following link</p>
+                    <break></break>
+                    <a href="http://localhost:4000/api/user/compromised/${user._id}">I didn't reset my password, help!</a>
+                `//mandarlo a frontend a una pagina de datos de contacto con una confirmación si quiere desabilitar su cuenta!
+                let mailOptions = {
+                    from: "Mar Del Casas <mardelcasas@gmail.com>",
+                    to: `${user.firstName} <${user.eMail}>`,
+                    subject: `Your password has changed ${user.firstName}!`,
+                    text: message,
+                    html: message
+                }
+                transporter.sendMail(mailOptions, (err, data) => {
+                    err ? res.json({success: true, response: 'password changed successfully but eMail failed to be sent'}) : res.json({success: true, response: 'password changed successfully & eMail sent'})
+                })
+            }else{
+                throw new Error('User not found')
+            }
+        })
+        .catch((err) => handleError(res, err))
+    }, // mandarle mail al usuario que hubo cambios en su cuenta, opcion de bannear cuenta. redirigir a un componente con datos de contacto.
+    disableUser: (req, res)=>{
+        console.log("Received Disable User because Compromised Petition:" + Date())
+        User.findOneAndUpdate({_id: req.params.id}, {banned: true})
+        .then(user => user ? res.json({success: true, response: 'user banned successfully'}) : res.json({success: false, response: 'user not found'}))
+        .catch(err => handleError(res, err))
+    },
+    manageUser: (req, res)=>{ // posible future feature, send mail for BANNED users only, consider appealing at help component
+        console.log("Received Manage User Petition:" + Date())
+        if(req.user.admin){
+            let whatToDo = req.body.actionToDo === 'ban'
+            User.findOneAndUpdate({_id: req.body._id}, {banned: whatToDo})
+            .then(user => user ? res.json({success: true, response: `User ${whatToDo ? 'banned' : 'unbanned'} successfully`}) : res.json({success: false, response: 'user not found'}))
+            .catch(err => handleError(res, err))
+        }else{
+            res.json({success: false, response: "You don't have permissions to do this"})
+        }
+    },
+    manageDreamHouseOfUser: (req, res)=>{
+        console.log("Received Manage Filter AND Email List Petition:" + Date())
+        let whatToDo = req.body.actionToDo === 'add'
+        User.findOneAndUpdate({_id: req.user._id}, {dreamProperty: whatToDo ? req.body.filter : {}, suscribedToNewsLetter: whatToDo && true})
+        .then(user => user ? res.json({success: true, response: `dream property ${whatToDo ? 'added to' : 'removed from'} user`}) : res.json({success: false, response: 'user not found'}))
+        .catch(err => handleError(res, err))
     }
 }
 
